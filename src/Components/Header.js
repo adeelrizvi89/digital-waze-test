8d78b3c36982498a04db50567ebd7e291a8c8a1c
Adeel Rizvi <adeelrizvi89@gmail.com>
2021-09-24 14:24:17 +0500

    Creater Header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,7 +1,31 @@
 import React from "react";
+import Avatar from "../Images/avatar.png";
 
 const Header = () => {
-  return <div className="header"></div>;
+  return (
+    <div className="header">
+      <div className="searchBar">
+        <i class="fas fa-search"></i>
+        <input type="text" placeholder="Search"></input>
+      </div>
+      <span className={{ fontsize: "16px", fontWeight: "500" }}>
+        Lorem ipsum
+      </span>
+      <span className={{ fontsize: "16px", fontWeight: "500" }}>
+        Lorem ipsum
+      </span>
+      <span className={{ fontsize: "16px", fontWeight: "500" }}>
+        Lorem ipsum
+      </span>
+      <i class="fas fa-comment"></i>
+      <i class="fas fa-bell"></i>
+      <div className="user">
+        <img src={Avatar}></img>
+        <span>John Smith</span>
+        <i class="fas fa-caret-down"></i>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
