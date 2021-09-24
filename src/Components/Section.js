2e04ba85c9dcd85cde6d2a3fb98b03af5a5b3a65
Adeel Rizvi <adeelrizvi89@gmail.com>
2021-09-24 16:45:31 +0500

    create about-curriculam-step

--- src/Components/Section.js
+++ src/Components/Section.js
@@ -40,6 +40,33 @@ const Section = () => {
             </p>
             <p>Apporoval Number : 22210 </p>
           </div>
+          <div className="curriculam-section">
+            <div className="curriculam-step">
+              <p>
+                Step<br></br>
+                <span
+                  style={{
+                    fontWeight: "bold",
+                    color: "black",
+                    fontSize: "20px",
+                  }}
+                >
+                  1
+                </span>
+                <br></br>24 May 2021<br></br>
+                <a href="#">create appoinment</a>
+              </p>
+              <div className="about-curriculam-step">
+                <h4>Psychoanalytic Couple Theraphy</h4>
+                <span style={{ fontSize: "14px", color: "#858585" }}>
+                  Lorem Ipsum is simply dummy text of the printing and
+                  typesetting industry. Lorem Ipsum has been the industry's
+                  standard dummy text ever since the 1500s,
+                </span>
+              </div>
+            </div>
+            <div></div>
+          </div>
         </section>
       </div>
     </div>
