0adcce7beec3939ec42a0f3eced5e5cc122442d1
Adeel Rizvi <adeelrizvi89@gmail.com>
2021-09-26 18:30:55 +0500

    rename component

--- src/Components/PageTwo.js
+++ src/Components/PageTwo.js
@@ -1,14 +1,23 @@
 import React from "react";
-import MainSection from "./MainSection";
-import Navbar from "./Navbar";
-import Header from "./Header";
-import ProgramAppoinment from "./ProgramAppoinment";
+import Button from "./Button";
+import Table from "./Table";
+import BusinessFlow from "./BusinessFlow";
+import WorkDropDown from "./WorkDropDown";
 
 const PageTwo = () => {
   return (
     <>
       <div className="container">
-        <ProgramAppoinment />
+        <div className="programeAppoinment-container">
+          <header>
+            <Button />
+            <WorkDropDown />
+          </header>
+          <section>
+            <Table />
+          </section>
+          <BusinessFlow />
+        </div>
       </div>
     </>
   );
